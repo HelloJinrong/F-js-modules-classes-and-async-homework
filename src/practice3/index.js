1190ed17ffa4fc7f3214f950b64d4a2132feeaf2
Jinrong He <hjr2027@163.com>
2020-11-11 10:11:01 +0800

    finish test

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,29 @@
-export const getPoetry
+/*
+export const getPoetry = async function () {
+    try {
+        const promise = await fetch("https://v1.jinrishici.com/all.json");
+        const data = await promise.json();
+        const res = [];
+        res.push(data.origin);
+        res.push(data.author);
+        res.push(data.content);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}; */
+
+export const getPoetry = async function () {
+  const res = [];
+  return fetch("https://v1.jinrishici.com/all.json", {
+    method: "GET",
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      res.push(data.origin);
+      res.push(data.author);
+      res.push(data.content);
+      return res;
+    })
+    .catch((error) => console.log(error));
+};
